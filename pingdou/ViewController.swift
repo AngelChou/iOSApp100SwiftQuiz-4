1d940b71098b427f0a1cc205f442642e57d767c4
Angel Chou <cat77120377@hotmail.com>
2018-10-22 11:59:18 +0800

    Initial Commit

---
+++ pingdou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pingdou
+//
+//  Created by Shun-Ching, Chou on 2018/10/22.
+//  Copyright © 2018 Shun-Ching, Chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
